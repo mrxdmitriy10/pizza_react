a1ceb00dfac156106d2a6a607d0c5c78766e96ec
mrxdmitriy10 <mrxdmitriy@icloud.com>
2024-12-06 20:37:21 +0500

    fix padding

--- src/3widgets/ItemPizza/ui/ItemPizza.tsx
+++ src/3widgets/ItemPizza/ui/ItemPizza.tsx
@@ -12,29 +12,24 @@ export const ItemPizza = () => {
     onepizza && (
       <div
         onClick={() => dispatch(resetSelectPizza())}
-        className="fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50 bg-blend-saturation"
+        className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50 bg-blend-saturation"
       >
         <div
           onClick={(e) => e.stopPropagation()}
-          className="flex w-full max-w-[800px] min-h-[400px] flex-col items-center justify-between gap-2 bg-white p-16 shadow-xl sm:flex-row sm:justify-start sm:py-10"
+          className="flex min-h-[400px] w-full sm:max-w-[800px] flex-col items-center justify-between gap-2 bg-white p-16 shadow-xl sm:flex-row sm:justify-start sm:py-10"
         >
           <div className="-mt-64 h-full pl-5">
             <img width={300} src={`${onepizza.img}`} alt="sds" />
           </div>
 
-
-
-
-
-          <div className="flex  flex-col items-center gap-5">
-          <div className="flex p-5 flex-col items-center  text-md font-light">
-            <div className="flex gap-3 justify-between text-orange-800 transition-all duration-300 ease-linear">
+          <div className="flex flex-col items-center gap-5 p-5">
+            <div className="text-md flex flex-col items-center  font-light">
+              <div className="flex justify-between gap-3 text-orange-800 transition-all duration-300 ease-linear">
                 {onepizza.ingridients.map((ingridient) => (
-                    <div>{ingridient}</div>
+                  <div>{ingridient}</div>
                 ))}
+              </div>
             </div>
-
-          </div>
             <div className="text-sm">{onepizza.description}</div>
             <div className="text-nowrap font-mono text-5xl font-extrabold text-orange-800 transition-all duration-300 ease-linear">
               {onepizza.name.toUpperCase()}
@@ -44,7 +39,7 @@ export const ItemPizza = () => {
               {onepizza.variant.map((params, index) => {
                 return (
                   <div
-                    key={index}
+                    key={index}fix
                     className="flex cursor-none flex-col items-center rounded-md px-3 py-3 hover:bg-orange-300 lg:py-0"
                     onClick={() =>
                       dispatch(
